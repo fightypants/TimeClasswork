2bc823bcd7f407e90248b3a3737158bdee01f38f
Cory Ribson & Kha Le <student@epicodus.com>
2015-04-21 10:30:10 -0700

    Added DST code

--- Time.rb
+++ Time.rb
@@ -9,3 +9,18 @@ class Time
     end
   end
 end
+
+class Time
+  define_method(:dayLightSaving) do
+    current_date = Time.new()
+    if self.dst?() == true && current_date.dst?() == false
+      "Daylight Savings Time is coming soon!"
+    elsif current_date.dst?() == true
+      "My clock is already set forward an hour"
+    elsif self.dst?() == false && current_date.dst() == false
+      "Don't reset your clocks yet!"
+    else
+      "Get more sleep!"
+    end
+  end
+end
